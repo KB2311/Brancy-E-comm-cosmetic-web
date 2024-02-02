7166d001442cb9b027b0287acebbeff968d46311
KB2311 <kunjbiharihirani@gmail.com>
2024-02-02 17:55:03 +0530

    kb

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Card = () => {
+  return <div>Card</div>;
+};
+
+export default Card;
+
+// const cards = [
+//   {
+//     title: 'Natural',
+//     imageUrl:
+//       'https://template.hasthemes.com/brancy/brancy/assets/images/shop/category/6.webp',
+//     className: 'bg-red-200 flex-grow flex-shrink-0 basis-44',
+//     badge: 'new',
+//   },
+//   {
+//     title: 'Hare Care',
+//     imageUrl:
+//       'https://template.hasthemes.com/brancy/brancy/assets/images/shop/category/1.webp',
+//     className: 'bg-blue-200 flex-grow flex-shrink-0 basis-44',
+//     badge: 'hot',
+//   },
+//   {
+//     title: 'Skin Care',
+//     imageUrl:
+//       'https://template.hasthemes.com/brancy/brancy/assets/images/shop/category/2.webp',
+//     className: 'bg-purple-200 flex-grow flex-shrink-0 basis-44',
+//   },
+//   {
+//     title: 'Test Care',
+//     imageUrl:
+//       'https://template.hasthemes.com/brancy/brancy/assets/images/shop/category/2.webp',
+//     className: 'bg-purple-200 flex-grow flex-shrink-0 basis-44',
+//   },
+// ];
+
+// Render your React component instead
