f6979ee8084532ed1ef11efd8146fc4ea2f7b52e
KB2311 <kunjbiharihirani@gmail.com>
2024-02-02 19:35:21 +0530

    hero page design

--- src/containers/Banner/index.jsx
+++ src/containers/Banner/index.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div className="min-h-screen">
+      <div className="hero flex items-center justify-center h-full md:pt-14">
+        <div className="px-2 grid w-full h-full grid-cols-1 md:grid-cols-2 items-center justify-center">
+          <div className="w-full h-full flex flex-col items-center justify-center text-center pt-24">
+            <div className="">
+              <img
+                className="object-contain"
+                src="	https://template.hasthemes.com/brancy/brancy/assets/images/slider/text-theme.webp"
+                alt=""
+                width={180}
+                height={97}
+              />
+            </div>
+            <div className="text-[32px] w-full tracking-wider font-bold text-[#231942]">
+              CLEAN FRESH
+            </div>
+            <div className="w-full mb-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis.
+            </div>
+            <div className="tracking-widest flex items-center border-2 rounded-full bg-white border-[#231942] text-[#231942] px-6 py-2">
+              BUY NOW
+            </div>
+          </div>
+
+          <div className="">2</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { IoSearchOutline } from 'react-icons/io5';
+import { FaBasketShopping } from 'react-icons/fa6';
+import { MdPerson } from 'react-icons/md';
+import { IoMenu } from 'react-icons/io5';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="fixed pt-3 md:pt-0 flex justify-between px-2 w-full">
+      <div className="h-16 flex items-center ">
+        <div className="md:w-40 cursor-pointer">
+          <img
+            className="h-12 object-fill"
+            src="https://template.hasthemes.com/brancy/brancy/assets/images/logo.webp"
+            alt=""
+          />
+        </div>
+        <div className="hidden lg:block w-fit flex items-start">
+          <ul className="flex gap-7 font-medium cursor-pointer">
+            <li className="hover:text-red-500">Home</li>
+            <li className="hover:text-red-500">About</li>
+            <li className="hover:text-red-500">Shop</li>
+            <li className="hover:text-red-500">Blog</li>
+            <li className="hover:text-red-500">Pages</li>
+            <li className="hover:text-red-500">Contact</li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex gap-5 items-center md:px-8">
+        <div className="flex gap-4">
+          <IoSearchOutline size={22} />
+          <FaBasketShopping size={20} />
+          <MdPerson size={23} />
+        </div>
+        <div className="lg:hidden flex items-center">
+          <IoMenu size={25} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
