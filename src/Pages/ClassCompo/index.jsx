041b7d486749650e208f7375471b1df63b51ccb5
KB2311 <kunjbiharihirani@gmail.com>
2024-02-10 23:44:04 +0530

    class compo update

--- src/Pages/ClassCompo/index.jsx
+++ src/Pages/ClassCompo/index.jsx
@@ -9,33 +9,75 @@ export default class ClassCompo extends Component {
      -> componentDidMount
     */
   // updating
+  /*
+     -> getDeriveStateFromProps
+     -> shouldComponentUpdate
+     -> render
+     -> getSnapshotBeforeUpdate
+     -> ComponentDidUpdate
+  */
   // unmounting
   // error
-  constructor() {
-    super();
+
+  constructor(props) {
+    super(props);
     this.state = {
       count: 0,
-      name: 'kunjbihari',
-    };
-    this.increment = () => {
-      this.setState(({ count }) => ({ count: count + 1 }));
-    };
-
-    this.decrement = () => {
-      this.setState(({ count }) => ({ count: count - 1 }));
-    };
-
-    this.changeName = () => {
-      this.setState({ name: 'kunjbihari hirani' });
+      name: 'kb',
+      data: null,
     };
   }
 
-  render() {
-    const { count, name } = this.state;
-    console.log(count);
+  async componentDidMount() {
+    window.addEventListener('dblclick', () => {
+      console.log('dblclick');
+    });
+    try {
+      const res = await fetch('https://fakestoreapi.com/products/1');
+      const json = await res.json();
+      this.setState({ data: json });
+      // console.log(json);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+  static getDerivedStateFromProps(props, state) {
+    return {
+      greet: `Heellooo from ${props.title}`,
+    };
+  }
+
+  increment = () => {
+    this.setState(({ count }) => ({ count: count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState(({ count }) => ({ count: count + 1 }));
+  };
+
+  changeName = () => {
+    this.setState({ name: 'Kunjbihari' });
+  };
+
+  render() {
+    const { count, name, greet, data } = this.state;
+    // const { title } = this.props;
     return (
-      <div className="flex flex-col py-40">
+      <div className="">
+        {data && <p className="text-2xl font-bold">{data.title}</p>}
+        <h1 className="text-3xl font-bold">{greet}</h1>
+        <button
+          className="w-fit border-2 border-black text-3xl font-bold"
+          type="button"
+          onClick={() => {
+            this.setState((state, props) => ({
+              greet: `hola from ${props.title}`,
+            }));
+          }}
+        >
+          Change Hello
+        </button>
         <div className=" flex items-center justify-center gap-10">
           <button
             className="flex h-10 w-10 items-center justify-center rounded-full bg-purple-700 text-2xl font-bold text-white"
