62797726c4ec053c549bd95f1add73e4a9a750a6
Kundan <kundanpatel2211@gmail.com>
2024-03-23 15:00:05 +0530

    Add Constants & Reducers also updated CartContext

---
+++ src/constants/actions.js
@@ -0,0 +1,8 @@
+export const LOAD_CART = 'LOAD_CART';
+export const ADD_CART = 'ADD_CART';
+export const UPDATE_CART = 'UPDATE_CART';
+export const DELETE_CART = 'DELETE_CART';
+
+export const REQUEST = 'REQUEST';
+export const SUCCESS = 'SUCCESS';
+export const FAIL = 'FAIL';
